f0e7c015597b462fcd56fc931afa4da1852d1ca0
DulceLuquin <168158685+DulceLuquin@users.noreply.github.com>
2024-04-28 01:54:31 -0700

    Add files via upload

---
+++ exam.js
@@ -0,0 +1,7 @@
+var nombre = prompt("Bienvenido, por favor ingrese su nombre:");
+
+if (nombre) {
+  alert("¡Hola, " + nombre + "! Bienvenido.");
+} else {
+  alert("No ingresaste ningún nombre. ¡Hola desconocido!");
+}
